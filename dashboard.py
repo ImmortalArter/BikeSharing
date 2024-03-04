78137f969534bcdc704cb44760f3208ee348ab87
ImmortalArter <160408380+ImmortalArter@users.noreply.github.com>
2024-03-04 23:20:15 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -36,14 +36,8 @@ Apakah terdapat perbedaan pola peminjaman antara pengguna casual dan pengguna te
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from sklearn.impute import SimpleImputer
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
 
 """## **Data Wrangling**
 
