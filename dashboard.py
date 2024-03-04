f0330b08652f4ba7b71482084afb3e9e83d0d9ab
ImmortalArter <160408380+ImmortalArter@users.noreply.github.com>
2024-03-04 23:10:41 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,11 +8,5 @@ from babel.numbers import format_currency
 # Load cleaned data
 day_df = pd.read_csv("day.csv")
 
-for column in datetime_columns:
-    day_df[column] = pd.to_datetime(day_df[column])
-
-# Filter data
-min_date = day_df["order_date"].min()
-max_date = day_df["order_date"].max()
 
 
